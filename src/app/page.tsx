926385cb6c6d7436495080fef06dc27fc3022953
Dlabm1973 <139680227+Dlabm1973@users.noreply.github.com>
2024-01-02 07:11:19 +0200

    Landing Page1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,8 +19,7 @@ export default function Home() {
         </span>
       </div>
       <p className="max-w-prose text-center">
-        An intelligent note-taking app with AI integration, built with OpenAI,
-        Pinecone, Next.js, Shadcn UI, Clerk, and more.
+        An intelligent note-taking app with AI integration.
       </p>
       <Button size="lg" asChild>
         <Link href="/notes">Open</Link>
